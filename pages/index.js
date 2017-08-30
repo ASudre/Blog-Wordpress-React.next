e40f3c9cf5bac05eed9455451c6a877e92fb84b2
Arthur Sudre <arthursudre@gmail.com>
2017-08-30 23:25:28 +0200

    feat(index): add flow

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
+// @flow
+
 import React from 'react'
+// $FlowIgnore unknown by flow
 import 'isomorphic-fetch'
 
-export default class MyPage extends React.Component {
-  static async getInitialProps (): string {
+import type { Post } from './types'
+
+type Props = {
+  posts: Post[]
+}
+
+const WORDPRESS_URL = 'http://localhost:8080';
+
+export default class MyPage extends React.Component<Props> {
+  static async getInitialProps (): Promise<{ posts: Post[] }> {
     // eslint-disable-next-line no-undef
-    const res = await fetch('http://localhost:8080/wp-json/wp/v2/posts')
-    const json = await res.json()
+    const res: Object = await fetch(`${WORDPRESS_URL}/wp-json/wp/v2/posts`)
+    const json: Post[] = await res.json()
     return { posts: json }
   }
 
