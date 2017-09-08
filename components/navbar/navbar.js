a2414f54827a67479df17ae19a5c38cbe7eb0219
Arthur Sudre <arthursudre@gmail.com>
2017-09-08 18:53:47 +0200

    feat(navbar): add responsiveness and style

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { Nav, NavItem, NavDropdown, DropdownItem, DropdownToggle, DropdownMenu, NavLink } from 'reactstrap';
+
+import { Collapse, DropdownToggle, DropdownMenu, DropdownItem, Navbar, NavbarToggler, NavbarBrand, NavDropdown, Nav, NavItem, NavLink } from 'reactstrap';
+
+const styles = {
+  primary: {
+    backgroundColor: '#e3f2fd',
+  },
+};
 
 export default class Example extends React.Component {
   constructor(props) {
     super(props);
 
     this.toggle = this.toggle.bind(this);
+    this.toggleDropDown = this.toggleDropDown.bind(this);
     this.state = {
+      isOpen: false,
       dropdownOpen: false,
     };
   }
-
   toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  toggleDropDown() {
     this.setState({
       dropdownOpen: !this.state.dropdownOpen,
     });
@@ -20,32 +34,32 @@ export default class Example extends React.Component {
   render() {
     return (
       <div>
-        <Nav tabs>
-          <NavItem>
-            <NavLink href="#" active>Link</NavLink>
-          </NavItem>
-          <NavDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-            <DropdownToggle nav caret>
-              Dropdown
-            </DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem header>Header</DropdownItem>
-              <DropdownItem disabled>Action</DropdownItem>
-              <DropdownItem>Another Action</DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>Another Action</DropdownItem>
-            </DropdownMenu>
-          </NavDropdown>
-          <NavItem>
-            <NavLink href="#">Link</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Another Link</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink disabled href="#">Disabled Link</NavLink>
-          </NavItem>
-        </Nav>
+        <Navbar style={styles.primary} light toggleable>
+          <NavbarToggler right onClick={this.toggle} />
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavDropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown}>
+                <DropdownToggle nav caret>
+                  Dropdown
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem header>Header</DropdownItem>
+                  <DropdownItem disabled>Action</DropdownItem>
+                  <DropdownItem>Another Action</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Another Action</DropdownItem>
+                </DropdownMenu>
+              </NavDropdown>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
     );
   }
