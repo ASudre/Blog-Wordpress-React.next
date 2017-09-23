bc0f5fccd4e991051e05d314b39451ca99207ef1
Arthur Sudre <arthursudre@gmail.com>
2017-09-23 14:36:48 +0200

    fix(mobile): change viewport

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -28,6 +28,7 @@ function layout({
     <div>
       <Head>
         <title>{title}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <style>{`
       body {
